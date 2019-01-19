b32588437830acc49d90aef7ac8490bb8d779285
limjisuu <dlawltn1999@naver.com>
2019-01-19 18:48:37 +0900

    함수 기반 뷰 (FBV) 의 4가지 패턴

--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,7 @@
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+from django.conf import settings
 
 # Create your views here.
 
@@ -12,3 +14,34 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분의 파이썬&장고 페이스메이커가 되어드리겠습니다.</p>
+'''.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    # filepath = 'c:\dev\askdjango\gdplev.xls'
+    filepath = os.path.join(settings.BASE_DIR, 'gdplev.xls')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
