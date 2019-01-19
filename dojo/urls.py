371191da410e3ddfab1590a6891eb904c620fe4e
limjisuu <dlawltn1999@naver.com>
2019-01-19 16:19:02 +0900

    dojo.views.mysum 에서 x/y/z 인자 3개를 처리

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
-
+from . import views
 urlpatterns = [
-
+    re_path(r'^sum/(?P<x>\d+)/$', views.mysum),
+    re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum)
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def mysum(request, x, y=0, z=0):
+    # request: HttpRequest
+    return HttpResponse(int(x) + int(y) + int(z))
