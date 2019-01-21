2f704b85a49284361d83a1adf1ee3df44b152bb1
limjisuu <dlawltn1999@naver.com>
2019-01-22 01:39:36 +0900

    url reverse 샘플 코드 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
 from . import views
 urlpatterns = [
-    re_path(r'^$', views.post_list),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^detail/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 ]
