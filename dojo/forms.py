83dd4224038b63d8f3bbfbc67f575eee557d20b4
limjisuu <dlawltn1999@naver.com>
2019-01-27 02:25:13 +0900

    24강 마무리

--- dojo/forms.py
+++ dojo/forms.py
@@ -7,7 +7,10 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'user_agent']
+        widgets = {
+            'user_agent': forms.HiddenInput,
+        }
 
 
     '''
