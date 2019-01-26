ac70f851fbdc79b8c4ab00aeaa952018da66e45f
limjisuu <dlawltn1999@naver.com>
2019-01-26 19:48:39 +0900

    dojo.models.Post 모델에 validators 적용하고, model_form.save(commit=False) 활용 예시 추가

--- dojo/views.py
+++ dojo/views.py
@@ -12,30 +12,9 @@ def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            #방법 1
-            #post = Post()
-            #post.title = form.cleaned_data['title']
-            #post.content = form.cleaned_data['content']
-            #post.save()
-
-            #빙법 2
-            '''
-            post = Post(title=form.cleaned_data['title'],
-                        content=form.cleaned_data['content'])
-
+            post = form.save(commit=False)
+            post.ip = request.META['REMOTE_ADDR']
             post.save()
-            '''
-
-            #방법 3
-            '''
-            post = Post.objects.create(title=form.cleaned_data['title'],
-                                        content=form.cleaned_data['content'])
-            '''
-
-            #방법 4
-            post = Post.objects.create(**form.cleaned_data)
-
-
             return redirect('/dojo/') #namespace:name
     else:
         form = PostForm()
