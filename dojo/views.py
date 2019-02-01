947b66b756bd0979f6b58c34814d39ce131b1766
limjisuu <dlawltn1999@naver.com>
2019-02-01 16:21:00 +0900

    CBV 샘플 - STEP 1. FBV

--- dojo/views.py
+++ dojo/views.py
@@ -8,6 +8,12 @@ from .forms import PostForm
 
 # Create your views here.
 
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'dojo/post_detail.html', {
+        'post': post,
+    })
+
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
