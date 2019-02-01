b0ab184e1519f8e0fd92eded8fa2b6cc7699b516
limjisuu <dlawltn1999@naver.com>
2019-02-02 03:51:43 +0900

    CBV : ListView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views_cbv
 app_name = 'blog' #에러 나서 구글링 해서 임의로 추가
 
 urlpatterns = [
-    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^$', views_cbv.post_list, name='post_list'),
     re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
     re_path(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
     path('new/', views.post_new, name='post_new'),
