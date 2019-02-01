b7e8c61fb771d869098d9e12430af6e6a85486ff
limjisuu <dlawltn1999@naver.com>
2019-02-02 03:56:37 +0900

    CBV : DetailView 적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,8 +1,10 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 from django import forms
 
 post_list = ListView.as_view(model=Post, paginate_by=10)
+
+post_detail = DetailView.as_view(model=Post)
 
 #blog/forms.py
 class PostForm(forms.ModelForm):
