83c163a35172ba35197043656c952e1624ea70db
limjisuu <dlawltn1999@naver.com>
2019-02-03 02:39:08 +0900

    회원가입 시에 이메일 추가 입력

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
 
 # Create your views here.
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL) #default: "/accounts/login/"
 
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup_form.html',
 {
         'form': form,
